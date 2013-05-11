409555be35dce08be59cebedac51294b7a9e185c
dhouk24 <dhouk24@gmail.com>
2013-05-10 21:34:23 -0500

    First attempt

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,20 @@
+
 require_relative '../../db/config'
 
 # implement your Student model here
+class Student < ActiveRecord::Base
+	validates :email, :format => {:with => /\w.+@.+\..{2}/, :message => "Only legit emails welcome"}, uniqueness: true
+	validates :age, :numericality => {:greater_than_or_equal_to => 5}
+	validates :phone, :format => {:with => /\d{3}.*\d{3}.*\d{4}.*/, :message => "Only valid phone numbers please"}
+
+	def name
+		first_name + ' ' + last_name
+	end
+
+	def age
+		now = Time.now
+		age = now.year - birthday.year
+		age -= 1 if now.yday < birthday.yday
+	end
+
+end
